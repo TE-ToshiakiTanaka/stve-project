c2df8b4422132a5631dad417ec6a26a8b1fae00e
Toshiaki.Tanaka <toshiaki.tanaka@jp.sony.com>
2016-03-29 15:40:03 +0900

    update.

--- utility.py
+++ utility.py
@@ -6,10 +6,8 @@ from stve.log import Log
 LOG = Log("Project.STVE")
 
 WORK_DIR = os.path.normpath(os.path.dirname(__file__))
-LIB_DIR = os.path.normpath(os.path.join(WORK_DIR, "library"))
+LIB_DIR = os.path.normpath(os.path.join(WORK_DIR, "lib"))
 SCRIPT_DIR = os.path.normpath(os.path.join(WORK_DIR, "script"))
 TMP_DIR = os.path.normpath(os.path.join(WORK_DIR, "tmp"))
 LOG_DIR = os.path.normpath(os.path.join(WORK_DIR, "log"))
 BIN_DIR = os.path.normpath(os.path.join(WORK_DIR, "bin"))
-
-DRIVER_DIR = os.path.normpath(os.path.join(BIN_DIR, "chromedriver"))
