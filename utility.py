fe157c0ce4225e5750ac22e815838e587de10bd9
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-03-30 01:14:30 +0900

    update.

--- utility.py
+++ utility.py
@@ -1,15 +1,21 @@
 import os
 import sys
+import logging
 
-from stve.log import Log
-
-LOG = Log("Project.STVE")
+from stve import log
 
 WORK_DIR = os.path.normpath(os.path.dirname(__file__))
-LIB_DIR = os.path.normpath(os.path.join(WORK_DIR, "library"))
+LIB_DIR = os.path.normpath(os.path.join(WORK_DIR, "lib"))
 SCRIPT_DIR = os.path.normpath(os.path.join(WORK_DIR, "script"))
 TMP_DIR = os.path.normpath(os.path.join(WORK_DIR, "tmp"))
 LOG_DIR = os.path.normpath(os.path.join(WORK_DIR, "log"))
 BIN_DIR = os.path.normpath(os.path.join(WORK_DIR, "bin"))
-
 DRIVER_DIR = os.path.normpath(os.path.join(BIN_DIR, "chromedriver"))
+
+LOG = log.Log("Project.STVE")
+logfile = os.path.join(LOG_DIR, "system.log")
+if not os.path.exists(logfile):
+    with open(logfile, 'a') as f:
+        os.utime(logfile, None)
+
+LOG.addHandler(log.Log.fileHandler(logfile, log.BASE_FORMAT, logging.DEBUG))
