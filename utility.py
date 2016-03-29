822985d0daad7cb63b96f4b0a64539931aebf06e
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-03-29 19:21:51 +0900

    update.

--- utility.py
+++ utility.py
@@ -1,9 +1,8 @@
 import os
 import sys
+import logging
 
-from stve.log import Log
-
-LOG = Log("Project.STVE")
+from stve import log
 
 WORK_DIR = os.path.normpath(os.path.dirname(__file__))
 LIB_DIR = os.path.normpath(os.path.join(WORK_DIR, "lib"))
@@ -11,3 +10,11 @@ SCRIPT_DIR = os.path.normpath(os.path.join(WORK_DIR, "script"))
 TMP_DIR = os.path.normpath(os.path.join(WORK_DIR, "tmp"))
 LOG_DIR = os.path.normpath(os.path.join(WORK_DIR, "log"))
 BIN_DIR = os.path.normpath(os.path.join(WORK_DIR, "bin"))
+
+LOG = log.Log("Project.STVE")
+logfile = os.path.join(LOG_DIR, "system.log")
+if not os.path.exists(logfile):
+    with open(logfile, 'a') as f:
+        os.utime(logfile, None)
+
+LOG.addHandler(log.Log.fileHandler(logfile, log.BASE_FORMAT, logging.DEBUG))
